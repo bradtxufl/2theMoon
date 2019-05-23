a403b5709793d213710cec28e5fad7eace00f279
bradtxufl <brad.w@utexas.edu>
2019-05-22 18:08:56 -0700

    everything works! I just need to do some color matching and extras

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 Workout Distance Tracking App
 
 todo:
-adjust percentage scrollbar
-bring in distance api from entered zip to Niagara Falls and Grand Canyon/populate their percentages
 create database of gif links and quotes to randomize on workout submit
-https://apexcharts.com/javascript-chart-demos/radialbar-charts/custom-angle-circle/
+try to match colors for buttons and progress bars
+change background based on weather conditions
--- scripts.js
+++ scripts.js
@@ -32,7 +32,11 @@ document.addEventListener('DOMContentLoaded', function () {
   var swimButton = document.querySelector('#swimButton')
   var bikeButton = document.querySelector('#bikeButton')
   var runButton = document.querySelector('#runButton')
-  var hype = document.querySelector('.hype')
+  var heckYes = document.querySelector('#heckYes')
+
+  heckYes.addEventListener('click', function () {
+    location.reload()
+  })
 
   var totalDist = localStorage.getItem('totDist') || 0
   totalDistance.innerHTML = `Your total distance: ${(JSON.parse(totalDist))} miles`
@@ -68,7 +72,25 @@ document.addEventListener('DOMContentLoaded', function () {
       let low = ((obj['main']['temp_min'] * (9/5)) - 459.67).toFixed(0)
       let current = ((obj['main']['temp_max'] * (9/5)) - 459.67).toFixed(0)
       temperatures.innerHTML = `Now: ${current}° | High: ${high}° | Low: ${low}°`
-      weatherImage.src = `${obj['weather'][0]['icon']}.jpeg`
+      if (obj['weather'][0]['icon'] == '01d') {
+        weatherImage.src = 'https://image.flaticon.com/icons/svg/136/136723.svg'
+    } else if (obj['weather'][0]['icon'] == '01n') {
+      weatherImage.src = 'https://image.flaticon.com/icons/svg/740/740878.svg'
+    } else if (obj['weather'][0]['icon'] == '02d' || obj['weather'][0]['icon'] == '03d' || obj['weather'][0]['icon'] == '04d') {
+      weatherImage.src = 'https://image.flaticon.com/icons/svg/861/861059.svg'
+    } else if (obj['weather'][0]['icon'] == '02n' || obj['weather'][0]['icon'] == '03n' || obj['weather'][0]['icon'] == '04n') {
+      weatherImage.src = 'https://image.flaticon.com/icons/svg/1113/1113763.svg'
+    } else if (obj['weather'][0]['icon'] == '09d' || obj['weather'][0]['icon'] == '10d' || obj['weather'][0]['icon'] == '09n' || obj['weather'][0]['icon'] == '10n') {
+      weatherImage.src = 'https://image.flaticon.com/icons/svg/414/414974.svg'
+    } else if (obj['weather'][0]['icon'] == '11d' || obj['weather'][0]['icon'] == '11n') {
+      weatherImage.src = 'https://image.flaticon.com/icons/svg/1684/1684363.svg'
+    } else if (obj['weather'][0]['icon'] == '13d' || obj['weather'][0]['icon'] == '13n') {
+      weatherImage.src = 'https://image.flaticon.com/icons/svg/642/642000.svg'
+    } else {
+      weatherImage.src = 'https://image.flaticon.com/icons/svg/1808/1808428.svg'
+    }
+    console.log(obj['weather'][0]['icon'])
+
       cityStateDisplay.innerHTML = obj['name']
     }
 
@@ -84,6 +106,59 @@ document.addEventListener('DOMContentLoaded', function () {
       })
   }
 
+  var canyonGrande = localStorage.getItem('gcfar') || 0
+
+  var gcDist = function (zippy) {
+    var url = `https://api.zip-codes.com/ZipCodesAPI.svc/1.0/CalculateDistance/ByZip?fromzipcode=${zippy}&tozipcode=86023&key=IA03X3J1TK051FCJXPMR`
+    function package (obj) {
+      canyonGrande = parseInt(obj['DistanceInMiles']).toFixed(0)
+      console.log(canyonGrande)
+      console.log(typeof canyonGrande)
+      localStorage.setItem('gcfar', canyonGrande)
+    }
+
+    fetch(url)
+      .then(function(response) {
+        return response.json()
+      })
+      .then(function(data) {
+        return JSON.stringify(data)
+      })
+      .then(function(string) {
+        package(JSON.parse(string))
+      })
+
+  }
+
+  grandCanyon.innerHTML = canyonGrande + ' miles'
+
+  var fallsNiag = localStorage.getItem('nffar') || 0
+
+  var nfDist = function (zippy) {
+    var url = `https://api.zip-codes.com/ZipCodesAPI.svc/1.0/CalculateDistance/ByZip?fromzipcode=${zippy}&tozipcode=14301&key=IA03X3J1TK051FCJXPMR`
+    function packageDos (obj) {
+      fallsNiag = parseInt(obj['DistanceInMiles']).toFixed(0)
+      console.log(fallsNiag)
+      console.log(typeof fallsNiag)
+      localStorage.setItem('nffar', fallsNiag)
+    }
+
+    fetch(url)
+      .then(function(response) {
+        return response.json()
+      })
+      .then(function(data) {
+        return JSON.stringify(data)
+      })
+      .then(function(string) {
+        packageDos(JSON.parse(string))
+      })
+
+  }
+
+  niagaraFalls.innerHTML = fallsNiag + ' miles'
+
+
   changeLocation.addEventListener('click', function (e) {
     localStorage.removeItem('storedZip')
     e.preventDefault()
@@ -94,6 +169,8 @@ document.addEventListener('DOMContentLoaded', function () {
     e.preventDefault()
     localStorage.setItem('storedZip', zipcodeField.value)
     zipcode = zipcodeField.value
+    gcDist(zipcode)
+    nfDist(zipcode)
     var url = `http://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&appid=a8e697a95390d4ae547c8e0dcaadae89`
     function weatherize (obj) {
       let high = ((obj['main']['temp'] * (9/5)) - 459.67).toFixed(0)
@@ -126,13 +203,8 @@ document.addEventListener('DOMContentLoaded', function () {
     function football (miles) {
       footballFields.innerHTML = (miles * 17.6).toFixed(0)
     }
-
-    function luna (miles) {
-      moon.innerHTML = `${(miles/2389).toFixed(4)}%`
-    }
-
     football(totalDist)
-    luna(totalDist)
+
 
     function progbar() {
       var green = (JSON.parse(hikeDist) / JSON.parse(totalDist)) * 101
@@ -145,7 +217,261 @@ document.addEventListener('DOMContentLoaded', function () {
       bbar.style = `width: ${yellow}%`
       rbar.style = `width: ${red}%`
     }
+
     progbar()
+
+    var graphSpace = function () {
+      var spaceDist = ((totalDist / 62) * 100).toFixed(0)
+      if (spaceDist > 100) {
+        return 100
+      } else {
+        return spaceDist
+      }
+    }
+
+    var graphGC = function () {
+      var numcg = JSON.parse(canyonGrande)
+      var gcpc = ((totalDist / numcg) * 100).toFixed(0)
+      if (gcpc > 100) {
+        return 100
+      } else {
+        return gcpc
+      }
+    }
+
+    var graphNF = function () {
+      var numfn = JSON.parse(fallsNiag)
+      var nfpc = ((totalDist / numfn) * 100).toFixed(0)
+      if (nfpc > 100) {
+        return 100
+      } else {
+        return nfpc
+      }
+    }
+
+    var graphMoon = (totalDist/2389).toFixed(4)
+
+    function graph() {
+      var options1 = {
+        chart: {
+          height: 280,
+          type: "radialBar",
+        },
+        series: [graphSpace(), graphGC(), graphNF(), graphMoon],
+        plotOptions: {
+          radialBar: {
+            dataLabels: {
+              total: {
+                show: false,
+                label: 'Get It'
+              }
+            }
+          }
+        },
+        labels: ['to Space', 'Grand Canyon', 'Niagara Falls', 'the Moon']
+      };
+
+      new ApexCharts(document.querySelector("#chart1"), options1).render();
+
+    }
+    graph()
+
+    function chartSpace() {
+      var options = {
+      chart: {
+        height: 280,
+        type: "radialBar"
+      },
+
+      series: [graphSpace()],
+      colors: ['#4d92ed'],
+
+      plotOptions: {
+        radialBar: {
+          hollow: {
+            margin: 15,
+            size: "70%"
+          },
+
+          dataLabels: {
+            showOn: "always",
+            name: {
+              offsetY: -10,
+              show: true,
+              color: "#fff",
+              fontSize: "13px"
+            },
+            value: {
+              color: "#fff",
+              fontSize: "30px",
+              show: true
+            }
+          }
+        }
+      },
+
+      stroke: {
+        lineCap: "round",
+      },
+      labels: ["To Space"]
+    };
+    var chart = new ApexCharts(document.querySelector("#chartspace"), options);
+
+    chart.render();
+
+    }
+    chartSpace()
+
+    function chartGC() {
+      var options = {
+      chart: {
+        height: 280,
+        type: "radialBar"
+      },
+
+      series: [graphGC()],
+      colors: ['#54f29e'],
+
+      plotOptions: {
+        radialBar: {
+          hollow: {
+            margin: 15,
+            size: "70%"
+          },
+
+          dataLabels: {
+            showOn: "always",
+            name: {
+              offsetY: -10,
+              show: true,
+              color: "#fff",
+              fontSize: "13px"
+            },
+            value: {
+              color: "#fff",
+              fontSize: "30px",
+              show: true
+            }
+          }
+        }
+      },
+
+      stroke: {
+        lineCap: "round",
+      },
+      labels: ["Grand Canyon"]
+    };
+    var chart = new ApexCharts(document.querySelector("#chartgc"), options);
+
+    chart.render();
+
+    }
+    chartGC()
+
+    function chartNF() {
+      var options = {
+      chart: {
+        height: 280,
+        type: "radialBar"
+      },
+
+      series: [graphNF()],
+      colors: ['#f7c145'],
+
+      plotOptions: {
+        radialBar: {
+          hollow: {
+            margin: 15,
+            size: "70%"
+          },
+
+          dataLabels: {
+            showOn: "always",
+            name: {
+              offsetY: -10,
+              show: true,
+              color: "#fff",
+              fontSize: "13px"
+            },
+            value: {
+              color: "#fff",
+              fontSize: "30px",
+              show: true
+            }
+          }
+        }
+      },
+
+      stroke: {
+        lineCap: "round",
+      },
+      labels: ["Niagara Falls"]
+    };
+    var chart = new ApexCharts(document.querySelector("#chartnf"), options);
+
+    chart.render();
+
+    }
+    chartNF()
+
+    function chartMoon() {
+      var options = {
+      chart: {
+        height: 280,
+        type: "radialBar"
+      },
+
+      series: [graphMoon],
+      colors: ['#f74444'],
+
+      plotOptions: {
+        radialBar: {
+          hollow: {
+            margin: 15,
+            size: "70%"
+          },
+
+          dataLabels: {
+            showOn: "always",
+            name: {
+              offsetY: -10,
+              show: true,
+              color: "#fff",
+              fontSize: "13px"
+            },
+            value: {
+              color: "#fff",
+              fontSize: "30px",
+              show: true
+            }
+          }
+        }
+      },
+
+      stroke: {
+        lineCap: "round",
+      },
+      labels: ["To the Moon"]
+    };
+
+
+
+    var chart = new ApexCharts(document.querySelector("#chartmoon"), options);
+
+    chart.render();
+
+    }
+    chartMoon()
+
+function update (totalDist) {
+  football(totalDist)
+  graph()
+  graphSpace()
+  chartMoon()
+  chartGC()
+  chartNF()
+  progbar()
+}
 
     hikeButton.addEventListener('click', function (e) {
       e.preventDefault()
@@ -155,12 +481,9 @@ document.addEventListener('DOMContentLoaded', function () {
       hikeDist = JSON.parse(hikeDist)
       hikeDist += parseInt(hikeInput.value)
       hikeDistance.innerHTML = `${JSON.parse(hikeDist)} miles`
-      hype.style.display = 'flex'
-      football(totalDist)
-      luna(totalDist)
+      update(totalDist)
       localStorage.setItem('hikeDist', JSON.stringify(hikeDist))
       localStorage.setItem('totDist', JSON.stringify(totalDist))
-      progbar()
     })
 
     swimButton.addEventListener('click', function (e) {
@@ -172,12 +495,9 @@ document.addEventListener('DOMContentLoaded', function () {
       swimDist = JSON.parse(swimDist)
       swimDist += parseInt(swimMiles)
       swimDistance.innerHTML = `${JSON.parse(swimDist)} miles`
-      hype.style.display = 'flex'
-      football(totalDist)
-      luna(totalDist)
+      update(totalDist)
       localStorage.setItem('swimDist', JSON.stringify(swimDist))
       localStorage.setItem('totDist', JSON.stringify(totalDist))
-      progbar()
     })
 
     bikeButton.addEventListener('click', function (e) {
@@ -188,12 +508,9 @@ document.addEventListener('DOMContentLoaded', function () {
       bikeDist = JSON.parse(bikeDist)
       bikeDist += parseInt(bikeInput.value)
       bikeDistance.innerHTML = `${JSON.parse(bikeDist)} miles`
-      hype.style.display = 'flex'
-      football(totalDist)
-      luna(totalDist)
+      update(totalDist)
       localStorage.setItem('bikeDist', JSON.stringify(bikeDist))
       localStorage.setItem('totDist', JSON.stringify(totalDist))
-      progbar()
     })
 
     runButton.addEventListener('click', function (e) {
@@ -204,11 +521,8 @@ document.addEventListener('DOMContentLoaded', function () {
       runDist = JSON.parse(runDist)
       runDist += parseInt(runInput.value)
       runDistance.innerHTML = `${JSON.parse(runDist)} miles`
-      hype.style.display = 'flex'
-      football(totalDist)
-      luna(totalDist)
+      update(totalDist)
       localStorage.setItem('runDist', JSON.stringify(runDist))
       localStorage.setItem('totDist', JSON.stringify(totalDist))
-      progbar()
     })
 })
